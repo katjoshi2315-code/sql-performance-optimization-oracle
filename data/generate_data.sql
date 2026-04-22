a9acce8f4d28e9b320ec6d892a391761b18909d6
katjoshi2315-code <katjoshi2315@gmail.com>
2026-04-22 15:33:51 +0530

    Add files via upload

--- data/generate_data.sql
+++ data/generate_data.sql
@@ -1,1 +1,19 @@
+-- Generate sample transaction data
 
+BEGIN
+    FOR i IN 1..100000 LOOP
+        INSERT INTO transactions VALUES (
+            i,
+            MOD(i, 10000),
+            SYSDATE - MOD(i, 365),
+            ROUND(DBMS_RANDOM.VALUE(10, 10000),2),
+            CASE WHEN MOD(i,2)=0 THEN 'CREDIT' ELSE 'DEBIT' END
+        );
+
+        IF MOD(i,10000)=0 THEN
+            COMMIT;
+        END IF;
+    END LOOP;
+    COMMIT;
+END;
+/
